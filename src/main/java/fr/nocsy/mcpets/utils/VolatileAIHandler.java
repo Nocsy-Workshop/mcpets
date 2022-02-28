f39ee24f807f839d44de30e63e0a8b6a5f2b4672
SaltyAimbOtter <saltyaimbotter@gmail.com>
2022-02-28 15:39:48 +0100

    adjust NMS usage to new pom dependencies

--- src/main/java/fr/nocsy/mcpets/utils/VolatileAIHandler.java
+++ src/main/java/fr/nocsy/mcpets/utils/VolatileAIHandler.java
@@ -10,20 +10,15 @@ import org.bukkit.craftbukkit.v1_18_R1.entity.CraftLivingEntity;
 
 public class VolatileAIHandler {
 
-    public static boolean navigateToLocation(AbstractEntity entity, AbstractLocation destination)
-    {
-        String version = Bukkit.getServer().getMinecraftVersion();
-
-        if(version.equals("1.18"))
-        {
+    public static boolean navigateToLocation(AbstractEntity entity, AbstractLocation destination) {
+        String version = Bukkit.getServer().getBukkitVersion();
+        if (version.equals("1.18.1-R0.1-SNAPSHOT")) {
             if (!entity.isLiving())
                 return false;
 
             EntityInsentient e = (EntityInsentient) ((CraftLivingEntity) BukkitAdapter.adapt(entity)).getHandle();
             e.D().a(destination.getX(), destination.getY(), destination.getZ(), 1.0D);
-        }
-        else
-        {
+        } else {
             MythicMobs.inst().getVolatileCodeHandler().getAIHandler().navigateToLocation(entity, destination, Double.POSITIVE_INFINITY);
         }
 
