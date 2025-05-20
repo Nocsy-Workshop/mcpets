e8b9d7dbb699b20db488afb33b3e6282a1dac225
Ax_Dev <96068728+AxDevv@users.noreply.github.com>
2025-05-20 06:49:40 +0200

    Add DismountOnDamagedExcludePlayers
    Useful to prevent players from dismounting another player from their mount

---
+++ GlobalConfig.java
@@ -0,0 +1,283 @@
+package fr.nocsy.mcpets.data.config;
+
+import fr.nocsy.mcpets.utils.PetAnnouncement;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GlobalConfig extends AbstractConfig {
+
+    public static GlobalConfig instance;
+
+    @Getter
+    @Setter
+    private boolean worldguardsupport = true;
+
+    @Getter
+    private String prefix;
+    @Getter
+    private String defaultName;
+    @Getter
+    private boolean overrideDefaultName;
+    @Getter
+    private int adaptiveInventory;
+    @Getter
+    private boolean useDefaultMythicMobNames;
+    @Getter
+    private boolean nameable;
+    @Getter
+    private boolean mountable;
+    @Getter
+    private boolean rightClickToOpen;
+    @Getter
+    private boolean leftClickToOpen;
+    @Getter
+    private boolean disableInventoryWhileHoldingSignalStick;
+    @Getter
+    private boolean sneakMode;
+    @Getter
+    private boolean dismountOnDamaged;
+    @Getter
+    private boolean dismountOnDamagedExcludePlayers;
+    @Getter
+    private boolean spawnPetOnReconnect;
+    @Getter
+    private boolean enableClickBackToMenu;
+    @Getter
+    private int distanceTeleport;
+    @Getter
+    private int maxNameLength;
+    @Getter
+    private boolean activateBackMenuIcon;
+
+    @Getter
+    private double percentHealthOnRespawn;
+    @Getter
+    private boolean autoRespawn;
+
+    @Getter
+    private int autoSave;
+
+    @Getter
+    private int experienceBarSize;
+    @Getter
+    private String experienceSymbol;
+    @Getter
+    private String experienceColorDone;
+    @Getter
+    private String experienceColorLeft;
+
+
+    @Getter
+    private boolean globalRespawnCooldown;
+    @Getter
+    private int defaultRespawnCooldown;
+
+    @Getter
+    private PetAnnouncement tamingAnnouncementType = PetAnnouncement.CHAT;
+    @Getter
+    private int tamingBarSize;
+    @Getter
+    private String tamingSymbol;
+    @Getter
+    private String tamingColorDone;
+    @Getter
+    private String tamingColorLeft;
+
+    @Getter
+    private boolean disableMySQL;
+    @Getter
+    private String MySQL_Prefix;
+    @Getter
+    private String MySQL_USER;
+    @Getter
+    private String MySQL_PASSWORD;
+    @Getter
+    private String MySQL_HOST;
+    @Getter
+    private String MySQL_PORT;
+    @Getter
+    private String MySQL_DB;
+    @Getter
+    private List<String> blackListedWorlds;
+
+    @Getter
+    @Setter
+    private boolean databaseSupport = false;
+
+    public static GlobalConfig getInstance() {
+        if (instance == null)
+            instance = new GlobalConfig();
+
+        return instance;
+    }
+
+    public void init() {
+        super.init("", "config.yml");
+
+        if (getConfig().get("Prefix") == null)
+            getConfig().set("Prefix", "§8[§6MCPets§8] » ");
+        if (getConfig().get("DefaultName") == null)
+            getConfig().set("DefaultName", "§9Pet of %player%");
+        if (getConfig().get("OverrideDefaultName") == null)
+            getConfig().set("OverrideDefaultName", true);
+        if (getConfig().get("EnableClickBackToMenu") == null)
+            getConfig().set("EnableClickBackToMenu", true);
+        if (getConfig().get("UseDefaultMythicMobsNames") == null)
+            getConfig().set("UseDefaultMythicMobsNames", false);
+        if (getConfig().get("RightClickToOpenMenu") == null)
+            getConfig().set("RightClickToOpenMenu", true);
+        if (getConfig().get("LeftClickToOpenMenu") == null)
+            getConfig().set("LeftClickToOpenMenu", true);
+        if (getConfig().get("DisableInventoryWhileHoldingSignalStick") == null)
+            getConfig().set("DisableInventoryWhileHoldingSignalStick", true);
+        if (getConfig().get("DismountOnDamaged") == null)
+            getConfig().set("DismountOnDamaged", true);
+        if (getConfig().get("DismountOnDamagedExcludePlayers") == null)
+            getConfig().set("DismountOnDamagedExcludePlayers", false);
+        if (getConfig().get("SpawnPetOnReconnect") == null)
+            getConfig().set("SpawnPetOnReconnect", true);
+        if (getConfig().get("SneakMode") == null)
+            getConfig().set("SneakMode", false);
+        if (getConfig().get("Nameable") == null)
+            getConfig().set("Nameable", true);
+        if (getConfig().get("Mountable") == null)
+            getConfig().set("Mountable", true);
+        if (getConfig().get("DistanceTeleport") == null)
+            getConfig().set("DistanceTeleport", 30);
+        if (getConfig().get("MaxNameLenght") == null)
+            getConfig().set("MaxNameLenght", 16);
+        if (getConfig().get("InventorySize") == null)
+            getConfig().set("InventorySize", -1);
+        if (getConfig().get("PercentHealthOnRespawn") == null)
+            getConfig().set("PercentHealthOnRespawn", 0.2);
+        if (getConfig().get("ActivateBackMenuIcon") == null)
+            getConfig().set("ActivateBackMenuIcon", true);
+        if (getConfig().get("AutoSaveDelay") == null)
+            getConfig().set("AutoSaveDelay", 3600);
+
+        if (getConfig().get("GlobalRespawnCooldown") == null)
+            getConfig().set("GlobalRespawnCooldown", false);
+        if (getConfig().get("DefaultRespawnCooldown") == null)
+            getConfig().set("DefaultRespawnCooldown", 0);
+        if (getConfig().get("AutoRespawn") == null)
+            getConfig().set("AutoRespawn", false);
+        if (getConfig().get("Experience.BarSize") == null)
+            getConfig().set("Experience.BarSize", 40);
+        if (getConfig().get("Experience.Symbol") == null)
+            getConfig().set("Experience.Symbol", "|");
+        if (getConfig().get("Experience.ColorDone") == null)
+            getConfig().set("Experience.ColorDone", "§a");
+        if (getConfig().get("Experience.ColorLeft") == null)
+            getConfig().set("Experience.ColorLeft", "§f");
+
+        if (getConfig().get("Taming.AnnouncementType") == null)
+            getConfig().set("Taming.AnnouncementType", PetAnnouncement.CHAT.name());
+        if (getConfig().get("Taming.BarSize") == null)
+            getConfig().set("Taming.BarSize", 40);
+        if (getConfig().get("Taming.Symbol") == null)
+            getConfig().set("Taming.Symbol", "|");
+        if (getConfig().get("Taming.ColorDone") == null)
+            getConfig().set("Taming.ColorDone", "§a");
+        if (getConfig().get("Taming.ColorLeft") == null)
+            getConfig().set("Taming.ColorLeft", "§f");
+
+        if (getConfig().get("MySQL.Prefix") == null)
+            getConfig().set("MySQL.Prefix", "");
+        if (getConfig().get("MySQL.User") == null)
+            getConfig().set("MySQL.User", "user");
+        if (getConfig().get("MySQL.Password") == null)
+            getConfig().set("MySQL.Password", "password");
+        if (getConfig().get("MySQL.Host") == null)
+            getConfig().set("MySQL.Host", "localhost");
+        if (getConfig().get("MySQL.Port") == null)
+            getConfig().set("MySQL.Port", "2560");
+        if (getConfig().get("MySQL.Database") == null)
+            getConfig().set("MySQL.Database", "mcpets_db");
+        if (getConfig().get("BlackListedWorlds") == null)
+            getConfig().set("BlackListedWorlds", new ArrayList<String>());
+        if (getConfig().get("DisableMySQL") == null)
+            getConfig().set("DisableMySQL", false);
+
+        save();
+        reload();
+    }
+
+    @Override
+    public void save() {
+        super.save();
+    }
+
+    @Override
+    public void reload() {
+        loadConfig();
+
+        prefix = getConfig().getString("Prefix");
+        defaultName = getConfig().getString("DefaultName");
+        overrideDefaultName = getConfig().getBoolean("OverrideDefaultName");
+        useDefaultMythicMobNames = getConfig().getBoolean("UseDefaultMythicMobsNames");
+        rightClickToOpen = getConfig().getBoolean("RightClickToOpenMenu");
+        leftClickToOpen = getConfig().getBoolean("LeftClickToOpenMenu");
+        disableInventoryWhileHoldingSignalStick = getConfig().getBoolean("DisableInventoryWhileHoldingSignalStick");
+        sneakMode = getConfig().getBoolean("SneakMode");
+        nameable = getConfig().getBoolean("Nameable");
+        mountable = getConfig().getBoolean("Mountable");
+        dismountOnDamaged = getConfig().getBoolean("DismountOnDamaged");
+        dismountOnDamagedExcludePlayers = getConfig().getBoolean("DismountOnDamagedExcludePlayers");
+        spawnPetOnReconnect = getConfig().getBoolean("SpawnPetOnReconnect");
+        distanceTeleport = getConfig().getInt("DistanceTeleport");
+        maxNameLength = getConfig().getInt("MaxNameLenght");
+        enableClickBackToMenu = getConfig().getBoolean("EnableClickBackToMenu");
+        activateBackMenuIcon = getConfig().getBoolean("ActivateBackMenuIcon");
+        adaptiveInventory = getConfig().getInt("InventorySize");
+        percentHealthOnRespawn = getConfig().getDouble("PercentHealthOnRespawn");
+        autoRespawn = getConfig().getBoolean("AutoRespawn");
+        defaultRespawnCooldown = Math.max(0, getConfig().getInt("DefaultRespawnCooldown"));
+        globalRespawnCooldown = getConfig().getBoolean("GlobalRespawnCooldown");
+        // Says it'll be an adaptive inventory
+        if (adaptiveInventory <= 0)
+            adaptiveInventory = -1;
+        // Inventory can't grow over 54
+        if (adaptiveInventory > 54)
+            adaptiveInventory = 54;
+        // Get the size of the inventory to place
+        while (adaptiveInventory > 0 && adaptiveInventory % 9 != 0 && adaptiveInventory < 54)
+            adaptiveInventory++;
+
+        autoSave = getConfig().getInt("AutoSaveDelay");
+
+        experienceBarSize = getConfig().getInt("Experience.BarSize");
+        experienceSymbol = getConfig().getString("Experience.Symbol");
+        experienceColorDone = getConfig().getString("Experience.ColorDone");
+        experienceColorLeft = getConfig().getString("Experience.ColorLeft");
+
+        tamingAnnouncementType = PetAnnouncement.get(getConfig().getString("Taming.AnnouncementType"));
+        tamingBarSize = getConfig().getInt("Taming.BarSize");
+        tamingSymbol = getConfig().getString("Taming.Symbol");
+        tamingColorDone = getConfig().getString("Taming.ColorDone");
+        tamingColorLeft = getConfig().getString("Taming.ColorLeft");
+
+        disableMySQL = getConfig().getBoolean("DisableMySQL");
+        MySQL_Prefix = getConfig().getString("MySQL.Prefix");
+        MySQL_USER = getConfig().getString("MySQL.User");
+        MySQL_PASSWORD = getConfig().getString("MySQL.Password");
+        MySQL_HOST = getConfig().getString("MySQL.Host");
+        MySQL_PORT = getConfig().getString("MySQL.Port");
+        MySQL_DB = getConfig().getString("MySQL.Database");
+        blackListedWorlds = getConfig().getStringList("BlackListedWorlds");
+    }
+
+    public boolean hasBlackListedWorld(String worldName) {
+        return blackListedWorlds.contains(worldName);
+    }
+
+    public boolean getBooleanField(String path) {
+        return getConfig().getBoolean(path);
+    }
+
+    public String getStringField(String path) {
+        return getConfig().getString(path);
+    }
+}
---
+++ PetListener.java
@@ -0,0 +1,411 @@
+package fr.nocsy.mcpets.listeners;
+
+import com.ticxo.modelengine.api.events.ModelDismountEvent;
+import com.ticxo.modelengine.api.events.ModelMountEvent;
+import fr.nocsy.mcpets.MCPets;
+import fr.nocsy.mcpets.PPermission;
+import fr.nocsy.mcpets.data.Items;
+import fr.nocsy.mcpets.data.Pet;
+import fr.nocsy.mcpets.data.PetDespawnReason;
+import fr.nocsy.mcpets.data.config.GlobalConfig;
+import fr.nocsy.mcpets.data.config.Language;
+import fr.nocsy.mcpets.data.flags.DismountPetFlag;
+import fr.nocsy.mcpets.data.flags.FlagsManager;
+import fr.nocsy.mcpets.data.inventories.PetInteractionMenu;
+import fr.nocsy.mcpets.data.livingpets.PetFood;
+import fr.nocsy.mcpets.data.sql.PlayerData;
+import fr.nocsy.mcpets.events.EntityMountPetEvent;
+import fr.nocsy.mcpets.events.PetOwnerInteractEvent;
+import fr.nocsy.mcpets.events.PetSpawnEvent;
+import fr.nocsy.mcpets.utils.Utils;
+import fr.nocsy.mcpets.utils.debug.Debugger;
+import io.lumine.mythic.bukkit.events.MythicMobDeathEvent;
+import io.lumine.mythic.bukkit.events.MythicMobDespawnEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityTameEvent;
+import org.bukkit.event.player.*;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class PetListener implements Listener {
+
+    private final HashMap<UUID, String> reconnectionPets = new HashMap<>();
+
+    @EventHandler
+    public void interact(PlayerInteractEntityEvent e) {
+        if (!GlobalConfig.getInstance().isRightClickToOpen())
+            return;
+
+        Player p = e.getPlayer();
+
+        if (GlobalConfig.getInstance().isSneakMode() && !p.isSneaking())
+            return;
+
+        // Do not open the menu if the player has a signal stick
+        if (GlobalConfig.getInstance().isDisableInventoryWhileHoldingSignalStick()) {
+            ItemStack it = p.getInventory().getItemInMainHand();
+            if (Items.isSignalStick(it))
+                return;
+        }
+
+        //If it's pet food in the main hand then do not open the menu
+        if (PetFood.getFromItem(p.getInventory().getItemInMainHand()) != null) {
+            return;
+        }
+
+        Entity ent = e.getRightClicked();
+
+        Pet pet = Pet.getFromEntity(ent);
+
+        if (pet != null && pet.getOwner() != null &&
+                pet.getOwner().equals(p.getUniqueId())) {
+            PetOwnerInteractEvent event = new PetOwnerInteractEvent(pet);
+            Utils.callEvent(event);
+            if (event.isCancelled()) return;
+
+            PetInteractionMenu menu = new PetInteractionMenu(pet, p.getUniqueId());
+            pet.setLastInteractedWith(p);
+            menu.open(p);
+        }
+        if (pet != null && p.isOp()) {
+            PetInteractionMenu menu = new PetInteractionMenu(pet, pet.getOwner());
+            pet.setLastOpInteracted(p);
+            menu.open(p);
+        }
+    }
+
+    @EventHandler
+    public void interact(EntityDamageByEntityEvent e) {
+        if (!GlobalConfig.getInstance().isLeftClickToOpen())
+            return;
+
+        if (!(e.getDamager() instanceof Player))
+            return;
+
+        Player p = (Player) e.getDamager();
+
+        if (GlobalConfig.getInstance().isSneakMode() && !p.isSneaking())
+            return;
+
+        Entity ent = e.getEntity();
+
+        Pet pet = Pet.getFromEntity(ent);
+
+        if (pet != null && pet.getOwner() != null &&
+                pet.getOwner().equals(p.getUniqueId())) {
+            PetInteractionMenu menu = new PetInteractionMenu(pet, p.getUniqueId());
+            pet.setLastInteractedWith(p);
+            menu.open(p);
+            e.setCancelled(true);
+            e.setDamage(0);
+        }
+        if (pet != null && p.isOp()) {
+            PetInteractionMenu menu = new PetInteractionMenu(pet, pet.getOwner());
+            pet.setLastOpInteracted(p);
+            menu.open(p);
+            e.setCancelled(true);
+            e.setDamage(0);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void disconnectPlayer(PlayerQuitEvent e) {
+        Player p = e.getPlayer();
+        Pet pet = Pet.getActivePets().get(p.getUniqueId());
+        if (pet != null) {
+            pet.despawn(PetDespawnReason.DISCONNECTION);
+            reconnectionPets.put(p.getUniqueId(), pet.getId());
+        }
+    }
+
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void reconnectionPlayer(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+
+        // delay before loading the player data from the database
+        Bukkit.getScheduler().runTaskLater(MCPets.getInstance(), () -> {
+            // Load the player data from the database for bungee support
+            if (GlobalConfig.getInstance().isDatabaseSupport()) {
+                PlayerData.reloadAll(p.getUniqueId());
+            }
+
+            if (reconnectionPets.containsKey(p.getUniqueId())) {
+                Pet pet = Pet.getFromId(reconnectionPets.get(p.getUniqueId()));
+                if (pet == null)
+                    return;
+                pet = pet.copy();
+                pet.setCheckPermission(false);
+                pet.setOwner(p.getUniqueId());
+                pet.spawn(p.getLocation(), true);
+                reconnectionPets.remove(p.getUniqueId());
+            }
+        }, 20L);
+    }
+
+    @EventHandler
+    public void teleport(PlayerChangedWorldEvent e) {
+        Player p = e.getPlayer();
+        if (Pet.getActivePets().containsKey(p.getUniqueId())) {
+            Pet pet = Pet.getActivePets().get(p.getUniqueId());
+            if (pet.getTamingProgress() < 1)
+                return;
+            pet.despawn(PetDespawnReason.TELEPORT);
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    pet.spawn(p, p.getLocation());
+                }
+            }.runTaskLater(MCPets.getInstance(), 20L);
+        }
+    }
+
+    @EventHandler
+    public void teleport(PlayerTeleportEvent e) {
+        Player p = e.getPlayer();
+        if (Pet.getActivePets().containsKey(p.getUniqueId())) {
+            Pet pet = Pet.getActivePets().get(p.getUniqueId());
+            pet.dismount(p);
+        }
+    }
+
+    @EventHandler
+    public void riding(EntityDamageEvent e) {
+        if (!GlobalConfig.getInstance().isDismountOnDamaged())
+            return;
+
+        if (e.getEntity() instanceof Player) {
+            if (e instanceof EntityDamageByEntityEvent) {
+                EntityDamageByEntityEvent edbe = (EntityDamageByEntityEvent) e;
+                if (edbe.getDamager() instanceof Player) {
+                    return;
+                }
+            }
+            
+            if (GlobalConfig.getInstance().isDismountOnDamagedExcludePlayers())
+                return;
+                
+            Player p = (Player) e.getEntity();
+            Pet pet = Pet.fromOwner(p.getUniqueId());
+            if (pet != null && pet.hasMount(p)) {
+                pet.dismount(p);
+            }
+        }
+    }
+
+    @EventHandler
+    public void damaged(EntityDamageEvent e) {
+        if (e.getEntity() instanceof Player) {
+            Pet pet = Pet.getFromEntity(e.getEntity());
+            // Cosmetic pets shouldn't be damageable
+            if (pet != null && pet.getPetStats() == null) {
+                e.setDamage(0);
+                e.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void gamemode(PlayerGameModeChangeEvent e) {
+        UUID uuid = e.getPlayer().getUniqueId();
+        if (Pet.getActivePets().containsKey(uuid) && e.getNewGameMode() == GameMode.SPECTATOR) {
+            Pet pet = Pet.getActivePets().get(uuid);
+            pet.despawn(PetDespawnReason.GAMEMODE);
+        }
+    }
+
+    /**
+     * Handle random despawn
+     */
+    private HashMap<UUID, Integer> repeatRespawn = new HashMap<>();
+    @EventHandler
+    public void despawn(MythicMobDespawnEvent e) {
+        if (e.getEntity() != null) {
+            Pet pet = Pet.getFromEntity(e.getEntity());
+            if (pet != null) {
+                if (!pet.isRemoved()) {
+                    pet.despawn(PetDespawnReason.MYTHICMOBS);
+                    UUID ownerUUID = pet.getOwner();
+                    if (ownerUUID != null) {
+                        Player owner = Bukkit.getPlayer(pet.getOwner());
+                        if (owner == null)
+                            return;
+                        if (repeatRespawn.containsKey(ownerUUID) && repeatRespawn.get(ownerUUID) == 3) {
+                            Language.REVOKED_UNKNOWN.sendMessage(owner);
+                            repeatRespawn.remove(owner.getUniqueId());
+                            return;
+                        }
+                        int value = 1;
+                        if (repeatRespawn.containsKey(ownerUUID))
+                            value = repeatRespawn.get(ownerUUID);
+                        pet.spawn(owner, owner.getLocation());
+                        pet.setRecurrent_spawn(false);
+                        repeatRespawn.put(owner.getUniqueId(), value + 1);
+                        new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                repeatRespawn.remove(owner.getUniqueId());
+                            }
+                        }.runTaskLater(MCPets.getInstance(), 10L);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Handle death of the pet
+     */
+    @EventHandler
+    public void death(MythicMobDeathEvent e) {
+        if (e.getEntity() != null) {
+            Pet pet = Pet.getFromEntity(e.getEntity());
+            if (pet != null) {
+                if (!pet.isRemoved()) {
+                    pet.despawn(PetDespawnReason.DEATH);
+                    if (pet.getOwner() != null) {
+                        Player owner = Bukkit.getPlayer(pet.getOwner());
+                        if (owner != null) {
+                            Language.REVOKED.sendMessage(owner);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Blacklisted world system
+     */
+    @EventHandler
+    public void blacklistedWorld(PetSpawnEvent e) {
+        if (GlobalConfig.getInstance().hasBlackListedWorld(e.getWhere().getWorld().getName())) {
+            e.setCancelled(true);
+            Player p = Bukkit.getPlayer(e.getPet().getOwner());
+            if (p != null) {
+                Language.BLACKLISTED_WORLD.sendMessage(p);
+            }
+        }
+    }
+
+    @EventHandler
+    public void despawnOnDismount(ModelDismountEvent e) {
+        if (e.getVehicle() == null || e.getVehicle().getModeledEntity() == null || e.getVehicle().getModeledEntity().getBase() == null)
+            return;
+
+        // Running this as sync coz we fetch an entity
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                Pet pet = Pet.getFromEntity(Bukkit.getEntity(e.getVehicle().getModeledEntity().getBase().getUUID()));
+                if (pet != null && pet.isDespawnOnDismount()) {
+                    pet.despawn(PetDespawnReason.DISMOUNT);
+                }
+            }
+        }.runTask(MCPets.getInstance());
+    }
+
+    @EventHandler
+    public void cancelDefaultTaming(EntityTameEvent e) {
+        if (Pet.getFromEntity(e.getEntity()) != null) {
+            // Cancel the event, so it doesn't give other type of item by default to the anchor
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void mountingPet(EntityMountPetEvent e) {
+        if (e.getEntity() == null)
+            return;
+
+        // if it's not the owner or an admin mounting the pet, then we cancel it
+        if (!e.getPet().getOwner().equals(e.getEntity().getUniqueId()) && !e.getEntity().hasPermission(PPermission.ADMIN.getPermission())) {
+            e.setCancelled(true);
+            Debugger.send("§c" + e.getEntity().getName() + " can not mount " + e.getPet().getId() + " as he's not the owner, nor an admin.");
+        }
+        // If user doesn't have the perm to mount the pet, cancel the event
+        if (e.getPet().getMountPermission() != null && !e.getEntity().hasPermission(e.getPet().getMountPermission())) {
+            e.setCancelled(true);
+            Language.CANT_MOUNT_PET_YET.sendMessage(e.getEntity());
+        }
+    }
+
+    @EventHandler
+    public void mountingPet(ModelMountEvent e) {
+        if (e.getPassenger() == null)
+            return;
+
+        if (e.getVehicle() == null || e.getVehicle().getModeledEntity() == null || e.getVehicle().getModeledEntity().getBase() == null)
+            return;
+
+        Entity entity;
+        try {
+            entity = Bukkit.getEntity(e.getVehicle().getModeledEntity().getBase().getUUID());
+        }
+        catch (Exception ex) {
+            entity = null;
+        }
+
+        if (entity == null)
+            return;
+        Pet pet = Pet.getFromEntity(entity);
+        Entity player = e.getPassenger();
+
+        if (pet == null)
+            return;
+
+        // if it's not the owner or an admin mounting the pet, then we cancel it
+        if (e.getSeat().isDriver() &&
+                !pet.getOwner().equals(player.getUniqueId()) &&
+                !player.hasPermission(PPermission.ADMIN.getPermission())) {
+            e.setCancelled(true);
+            Debugger.send("[ModelMountEvent] §c" + player.getName() + " can not mount model of " + pet.getId() + " as he's not the owner, nor an admin.");
+        }
+
+        if (GlobalConfig.getInstance().isWorldguardsupport() &&
+                FlagsManager.getFlag(DismountPetFlag.NAME) != null &&
+                FlagsManager.getFlag(DismountPetFlag.NAME).testState(player.getLocation())) {
+            e.setCancelled(true);
+            Debugger.send("§c" + player.getName() + " can not mount model of " + pet.getId() + " as a region is preventing mounting.");
+            Language.NOT_MOUNTABLE_HERE.sendMessage(player);
+            if (pet.isDespawnOnDismount())
+                pet.despawn(PetDespawnReason.FLAG);
+            return;
+        }
+
+        // If user doesn't have the perm to mount the pet, cancel the event
+        if (pet.getMountPermission() != null
+                && !player.hasPermission(pet.getMountPermission())
+                && e.getSeat().isDriver()) {
+            e.setCancelled(true);
+            Language.CANT_MOUNT_PET_YET.sendMessage(player);
+        }
+    }
+
+    @EventHandler
+    public void checkFlagMount(EntityMountPetEvent e) {
+        Pet pet = e.getPet();
+        Entity player = e.getEntity();
+        if (player instanceof Player &&
+                GlobalConfig.getInstance().isWorldguardsupport() &&
+                FlagsManager.getFlag(DismountPetFlag.NAME) != null &&
+                FlagsManager.getFlag(DismountPetFlag.NAME).testState(player.getLocation())) {
+            e.setCancelled(true);
+            Debugger.send("[EntityMountPetEvent] §c" + player.getName() + " can not mount model of " + pet.getId() + " as a region is preventing mounting.");
+            Language.NOT_MOUNTABLE_HERE.sendMessage(player);
+            if (pet.isDespawnOnDismount())
+                pet.despawn(PetDespawnReason.FLAG);
+        }
+    }
+}
